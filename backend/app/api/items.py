f22aedc40151633e02dd3385400ac90fdb298646
mombayashi <monbayashi00@yahoo.co.jp>
2025-07-10 11:19:25 +0900

    add description and refactoring

--- backend/app/api/items.py
+++ backend/app/api/items.py
@@ -1,7 +1,6 @@
 import os
-import requests
 from dotenv import load_dotenv
-from fastapi import APIRouter, Query, HTTPException
+from fastapi import APIRouter, HTTPException
 from app.services.rakuten_api import get_rakuten__api_items
 
 
--- backend/app/api/sentiment.py
+++ backend/app/api/sentiment.py
@@ -1,11 +1,11 @@
 from fastapi import APIRouter, HTTPException
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 from app.services.sentiment_api import analyze_sentiment
 
 router = APIRouter()
 
 class SentimentRequest(BaseModel):
-    text: str
+    text: str = Field(..., description="感情分析を行いたいテキスト")
 
 @router.post("/")
 def get_sentiment(request: SentimentRequest):
